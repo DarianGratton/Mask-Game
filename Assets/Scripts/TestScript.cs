2233cae741450b82c255ee3bf5136eec1951b34e
voidprophet <kentmcd2568@gmail.com>
2026-01-31 00:12:12 -0800

    Opened the unity file and created Visual Assets folder and Scripts folder with a test script.

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Testing Github Push and Scripting.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
