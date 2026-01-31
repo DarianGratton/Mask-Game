0ae8ff33941e855e23644bdb8ef0ce4e4e4caf6f
Colt King <pokedudecolt@yahoo.ca>
2026-01-31 15:30:53 -0800

    Added oxygen gauge that depletes and shows a death message once it runs out before resetting. (in ColtScene)

--- Assets/Scripts/Oxygen.cs
+++ Assets/Scripts/Oxygen.cs
@@ -3,15 +3,23 @@ using UnityEngine;
 public class Oxygen : MonoBehaviour
 {
     public float MAX_OXYGEN = 100f;
-    public float OXYGEN_LOSS_PER_SECOND = 1f;
+    public float OXYGEN_LOSS_PER_SECOND = 5f;
+    public float DEATH_MESSAGE_DURATION = 2f;
     public bool IS_LOSING_OXYGEN_ON_START = true;
-    
+
+    public Canvas DEATH_MESSAGE_CANVAS;
+
+    [SerializeField]
     private float currentOxygen;
     private bool isLosingOxygen;
+    [SerializeField]
+    private float deathMessageTimer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        deathMessageTimer = 0;
+        DEATH_MESSAGE_CANVAS.gameObject.SetActive(false);
         currentOxygen = MAX_OXYGEN;
         isLosingOxygen = IS_LOSING_OXYGEN_ON_START;
     }
@@ -20,10 +28,17 @@ public class Oxygen : MonoBehaviour
     void Update()
     {
         if (isLosingOxygen)
-            currentOxygen -= Time.deltaTime;
+            currentOxygen -= Time.deltaTime * OXYGEN_LOSS_PER_SECOND;
         
         if (currentOxygen <= 0)
             KillPlayer();
+
+        if (deathMessageTimer > 0)
+        {
+            deathMessageTimer -= Time.deltaTime;
+            if (deathMessageTimer <= 0)
+                DEATH_MESSAGE_CANVAS.gameObject.SetActive(false);
+        }
     }
 
     private void FixedUpdate()
@@ -33,7 +48,9 @@ public class Oxygen : MonoBehaviour
     
     private void KillPlayer()
     {
-        
+        DEATH_MESSAGE_CANVAS.gameObject.SetActive(true);
+        deathMessageTimer = DEATH_MESSAGE_DURATION;
+        currentOxygen = MAX_OXYGEN;
     }
     
     public void StopLosingOxygen()
@@ -53,5 +70,10 @@ public class Oxygen : MonoBehaviour
         if (currentOxygen > MAX_OXYGEN)
             currentOxygen = MAX_OXYGEN;
             
+    }
+
+    public float GetOxygenPercent()
+    {
+        return currentOxygen / MAX_OXYGEN;
     }
 }
