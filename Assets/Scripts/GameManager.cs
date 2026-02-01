9de08a5e1c79a640873f13c83bc858c359c4d144
Darian G <25462660+DarianGratton@users.noreply.github.com>
2026-01-31 18:14:37 -0800

    Add more corpse stuff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,12 +17,17 @@ public class GameManager : MonoBehaviour
     [Header("Corpse Persistence")]
     [SerializeField] GameObject corpseObject;
     [SerializeField] int maxNumberOfCorpses = 5;
-    List<GameObject> playerCorpses = new List<GameObject>(); 
+    List<GameObject> playerCorpses = new List<GameObject>();
+
+    private Vector3 startingSpawnPos;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        SpawnPlayer();
+        //SpawnPlayer();
+
+        startingSpawnPos = startingSpawn.transform.position;
+        startingSpawnPos.y += spawnOffsetY;
     }
 
     public void ResetPlayer()
@@ -33,15 +38,15 @@ public class GameManager : MonoBehaviour
 
     public void SpawnPlayer()
     {
-        Vector3 playerPosition = startingSpawn.transform.position;
-        playerPosition.y += spawnOffsetY;       
-        player.transform.position = playerPosition;
+        Vector3 spawnPosition = startingSpawn.transform.position;
+        spawnPosition.y += spawnOffsetY;       
+        player.transform.position = startingSpawnPos;
         player.transform.rotation = Quaternion.identity;
     }
 
     public void SpawnCorpse()
     {
-        GameObject newCorpse = Instantiate(corpseObject, player.transform.localPosition, player.transform.rotation);
+        GameObject newCorpse = Instantiate(corpseObject, player.transform.position, Quaternion.identity);
         if (playerCorpses.Count >= maxNumberOfCorpses)
         {
             GameObject playerCorpse = playerCorpses[0];
--- Assets/Scripts/Oxygen.cs
+++ Assets/Scripts/Oxygen.cs
@@ -30,9 +30,11 @@ public class Oxygen : MonoBehaviour
     {
         if (isLosingOxygen)
             currentOxygen -= Time.deltaTime * OXYGEN_LOSS_PER_SECOND;
-        
+
         if (currentOxygen <= 0)
+        {
             KillPlayer();
+        }
 
         if (deathMessageTimer > 0)
         {
@@ -52,6 +54,8 @@ public class Oxygen : MonoBehaviour
         DEATH_MESSAGE_CANVAS.gameObject.SetActive(true);
         deathMessageTimer = DEATH_MESSAGE_DURATION;
         currentOxygen = MAX_OXYGEN;
+        Time.timeScale = 0; // Pause Game
+        Cursor.visible = true;
 
         // Reset player position and spawn corpse.
         GAME_MANAGER.ResetPlayer();
