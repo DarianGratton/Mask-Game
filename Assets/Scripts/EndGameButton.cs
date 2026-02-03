4853c64b4e5b87dcf5038f22ca215ccf515b5fed
Colt King <king.colt@yahoo.ca>
2026-02-02 21:23:30 -0800

    added reactor hum added light in reactor fixed yellow skybox added death plane on tentacles removed keycard on axe fixed fall damage added end animation allowed player to put multiple plugs in at once

--- Assets/Scripts/EndGameButton.cs
+++ Assets/Scripts/EndGameButton.cs
@@ -1,18 +1,24 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class EndGameButton : MonoBehaviour
 {
     public Canvas endGameCanvas;
-
-
+    public PlayerSounds PLAYER_SOUNDS;
+    public Canvas uiCanvas;
+    public Canvas transitionCanvas;
+    public VideoPlayer backgroundVideo;
 
     public void endGame()
     {
-        endGameCanvas.gameObject.SetActive(true);
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
-        Debug.Log("Contact made");
+        //endGameCanvas.gameObject.SetActive(true);
+        //Cursor.lockState = CursorLockMode.None;
+        //Cursor.visible = true;
+        //Debug.Log("Contact made");
+
+        StartCoroutine(PlayEnding());
     }
 
         
@@ -21,5 +27,24 @@ public class EndGameButton : MonoBehaviour
         endGameCanvas.gameObject.SetActive(false);
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = false;
+    }
+
+    IEnumerator PlayEnding()
+    {
+        PLAYER_SOUNDS.KillPlayerSounds();
+
+        uiCanvas.gameObject.SetActive(false);
+
+        transitionCanvas.gameObject.SetActive(true);
+        VideoPlayer transitionPlayer = transitionCanvas.GetComponentInChildren<VideoPlayer>();
+
+        if (transitionPlayer)
+            transitionPlayer.Play();
+        else
+            Debug.Log("NO VIDEO PLAYER");
+
+        yield return new WaitForSeconds(6);
+
+        SceneManager.LoadScene("MainMenu");
     }
 }
